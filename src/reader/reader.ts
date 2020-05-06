a98afe901843c68ea6a804dd1f909a3eb7b18640
smasiu <szymon.masko.dev@gmail.com>
2020-05-06 10:11:24 +0200

    read path math

--- src/reader/reader.ts
+++ src/reader/reader.ts
@@ -1,10 +1,17 @@
 import { DingoConfig } from "../config/config.type";
 import { DocsMaper } from "../docs-maper/docs-maper";
-import { aReadDir, aLstat } from "../async-fs/async-fs";
-import { Stats } from "fs";
-import { join } from 'path';
 import { internalError } from "../errors/error-exceptions";
 import glob from 'glob';
+
+interface FoldersMatch {
+    simple: string[];
+    multiple: string[];
+}
+
+interface FileMatch {
+    ext: string[];
+    filePath: string[];
+}
 
 export class Reader {
     
@@ -19,8 +26,6 @@ export class Reader {
         return new Promise(async (resolve, reject) => {
             try {
                 let maper = new DocsMaper();
-
-                // await this.readEntryRecursive(maper, this.path);
 
                 await this.getFilePaths();
             } catch {
@@ -38,6 +43,10 @@ export class Reader {
 
     getFilePaths(): Promise<string[]> {
         return new Promise((resolve, reject) => {
+            
+            console.log(this.getFileMath())
+            console.log(this.getFolderMath())
+
             glob('!(node_modules|docs)/**/!(*.d.ts|*.css)',{
                 nodir: true,
             }, (err: Error | null, files: string[]) => {
@@ -49,6 +58,31 @@ export class Reader {
                 return resolve(files);
             });
         });
+    }
+
+    getFileMath(): FileMatch {
+
+        const { exclude } = this.config.entry.files;
+        const files: FileMatch = { ext: [], filePath: [] };
+
+        exclude.forEach( e => {
+            files[e?.[0] === '*' ? 'ext' : 'filePath'].push(e);
+        });
+
+        return files;
+    }
+
+    getFolderMath(): FoldersMatch {
+
+        const { exclude } = this.config.entry.paths;
+        const folders: FoldersMatch = { simple: [], multiple: [] };
+
+        exclude.forEach( e => {
+            folders[e.search(/\//) !== -1 ? 'multiple' : 'simple'].push(e);
+        });
+
+        return folders;
+
     }
 
 }
